3466b803e18b5ae61a74c653448da6e6333d4903
Chris Hayduk <chrishayduk@chriss-mac.local>
2015-12-31 01:44:11 -0500

    Completed app

---
+++ SettingsViewController.swift
@@ -0,0 +1,65 @@
+//
+//  SettingsViewController.swift
+//  tips
+//
+//  Created by Chris Hayduk on 12/29/15.
+//  Copyright © 2015 Chris Hayduk. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var defaultTip: UISegmentedControl!
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let tipPercentage = defaults.doubleForKey("default_tip_percentage")
+        
+        if (tipPercentage == 0.1) {
+            defaultTip.selectedSegmentIndex = 0
+        }
+        else if (tipPercentage == 0.15){
+            defaultTip.selectedSegmentIndex = 1
+        }
+        else if (tipPercentage == 0.2) {
+            defaultTip.selectedSegmentIndex = 2
+        }
+        else {
+            defaultTip.selectedSegmentIndex = 1
+        }
+    }
+
+    @IBAction func valueChanged(sender: AnyObject) {
+        let percentages = [0.1, 0.15, 0.2]
+        let index = defaultTip.selectedSegmentIndex
+        let percentageSelected = percentages[index]
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setDouble(percentageSelected, forKey: "default_tip_percentage")
+        defaults.synchronize()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -10,16 +10,74 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var numOfPeopleField: UITextField!
+ 
+    
+   override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
+   }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
     }
 
-
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let tipPercentage = defaults.doubleForKey("default_tip_percentage")
+        
+        if (tipPercentage == 0.1) {
+            tipControl.selectedSegmentIndex = 0
+        }
+        else if (tipPercentage == 0.15){
+            tipControl.selectedSegmentIndex = 1
+        }
+        else if (tipPercentage == 0.2) {
+            tipControl.selectedSegmentIndex = 2
+        }
+        else {
+            tipControl.selectedSegmentIndex = 1
+        }
+        
+        let numPeople = NSString(string: numOfPeopleField.text!).doubleValue
+        let billAmount = NSString(string: billField.text!).doubleValue
+        let tip = billAmount * tipPercentage
+        let total = (billAmount + tip)/numPeople
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        var tipPercentages = [0.10, 0.15, 0.20]
+        let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        let numPeople = NSString(string: numOfPeopleField.text!).doubleValue
+        let billAmount = NSString(string: billField.text!).doubleValue
+        let tip = billAmount * tipPercentage
+        let total = (billAmount + tip)/numPeople
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 }
 
